146b98c5a4097d0d7cc2a356c0f0630bc404563f
jochemlam <skyherojochempie@gmail.com>
2021-11-24 13:08:30 +0100

    uploaded bland project with navigation options

---
+++ PresentStudent/src/main/java/com/presentstudent/presentstudent/Application.java
@@ -0,0 +1,24 @@
+package com.presentstudent.presentstudent;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Application extends javafx.application.Application {
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("home.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 400, 600);
+        primaryStage.setTitle("Hoofdscherm");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
---
+++ PresentStudent/src/main/java/com/presentstudent/presentstudent/Controller.java
@@ -0,0 +1,81 @@
+package com.presentstudent.presentstudent;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Controller {
+
+    @FXML
+    void goAbsent(ActionEvent event) throws IOException {
+        changeScene(event, "absent", "Absentie aanmaken");
+    }
+
+    @FXML
+    void goAbsentOverview(ActionEvent event) throws IOException {
+        changeScene(event, "overzicht", "Absentie overzicht");
+    }
+
+    @FXML
+    void goHome(ActionEvent event) throws IOException {
+        changeScene(event, "home", "Hoofdmenu");
+    }
+
+    @FXML
+    void goSchedule(ActionEvent event) throws IOException {
+        changeScene(event, "rooster", "Rooster overzicht");
+    }
+
+    @FXML
+    void lastWeek(ActionEvent event) throws IOException {
+
+    }
+
+    @FXML
+    void thisWeek(ActionEvent event) throws IOException {
+
+    }
+
+    @FXML
+    void nextWeek(ActionEvent event) throws IOException {
+
+    }
+
+    @FXML
+    void sync(ActionEvent event) throws IOException {
+
+    }
+
+    @FXML
+    void scan(ActionEvent event){
+
+    }
+
+    @FXML
+    void sendAbsent(ActionEvent event) throws IOException {
+        // code voor verzenden hier
+
+
+
+        // dit is om terug te gaan naar het vorige scherm.
+        goAbsentOverview(event);
+    }
+
+    @FXML
+    void changeScene(ActionEvent event, String filename, String titleName) throws IOException {
+
+        Parent parent = FXMLLoader.load(getClass().getResource(filename + ".fxml"));
+        Scene scene = new Scene(parent);
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setTitle(titleName);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+}
